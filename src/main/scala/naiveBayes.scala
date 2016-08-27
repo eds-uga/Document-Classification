3b6a73b1037c7a6cb8cd18db60c9d530953be5fb
musmannisar <usman@uga.edu>
2016-08-27 11:55:42 -0400

    almost working combinebykey

--- src/main/scala/naiveBayes.scala
+++ src/main/scala/naiveBayes.scala
@@ -1,4 +1,11 @@
+import java.util
+
+import com.github.fommil.netlib.BLAS
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+import scala.collection.immutable.HashMap
+import scala.collection.mutable
 
 /**
   * Created by UNisar on 8/20/2016.
@@ -22,7 +29,7 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     * This is a map(x1) of map(x2)[String, Int], where x1 is the class, x2 is the word and Int is the count
     */
   def train() {
-      finalTrainingResult = finalval.collectAsMap()
+//    finalTrainingResult = finalval.collectAsMap()
   }
 
   /**
@@ -30,7 +37,7 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     */
   def classify(): Unit =
   {
-    testData.map ( document => getScoreForTargetType(document.split("""[\s\W]""").filter (_.length>0).map(_.toLowerCase))).saveAsTextFile("Results")
+//    testData.map ( document => getScoreForTargetType(document.split("""[\s\W]""").filter (_.length>0).map(_.toLowerCase))).saveAsTextFile("Results")
   }
 
   //region Helper Methods
@@ -59,7 +66,7 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
   //region DATA reading stuff, pretty straightforward
 
   val termDocsRdd = sc.textFile(x)
-//  val vocabulary = termDocsRdd.flatMap(y => y.split(" ")).map(Word(_)).distinct().collect()
+  //  val vocabulary = termDocsRdd.flatMap(y => y.split(" ")).map(Word(_)).distinct().collect()
   val vocabularySize = termDocsRdd.flatMap(y => y.split(" ")).distinct().count()
   val sentinelValue = 1.0/vocabularySize
   val xStream = sc.textFile(x)
@@ -73,22 +80,69 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
   val finalStream =  for (x <- finalout)  yield new Tuple2(x._2.head, x._1.head)
 
   val targetClasses = Set("CCAT", "ECAT", "GCAT", "MCAT")
-  val data = finalStream.flatMap ( x =>
+  val data:RDD[(String, Vector[String])] = finalStream.flatMap ( x =>
   {
-    val targetTypes = x._1.split(",")
-    if (targetTypes.toSet.intersect(targetClasses).isEmpty)
+    val targetTypes =  x._1.split(",").toSet.intersect(targetClasses)
+    if (targetTypes.isEmpty)
       None
     else
-      targetTypes.map ( b => (b, x._2))
-  }).reduceByKey(_+ " " +_)
+      targetTypes.map ( b => (b, x._2.split(" ").toVector))
+  })
 
-  val finalval = data.filter (x => targetClasses.contains(x._1)).map ( x => (x._1, x._2.split("""[\s\W]""").filter (_.length > 0).map (_.toLowerCase).foldLeft(Map.empty[String, Int]) {
-    (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
-  }))
+  def getMap(vals: Vector[String]): mutable.HashMap[String, Int] = {
+    val map = new mutable.HashMap[String, Int]()
+    vals.foreach ( v => map.put(v, 1))
+    map
+  }
+
+  def mergeMaps(output: mutable.HashMap[String, Int], input: Vector[String]): mutable.HashMap[String, Int] = {
+    input.foreach ( v => output.put(v, output.getOrElse(v, 0) + 1))
+    output
+  }
+
+  def createCombiner(vals: Vector[String]): (Long, mutable.HashMap[String, Int]) = {
+    (1L, getMap(vals))
+  }
+
+  def mergeValue(c: (Long, mutable.HashMap[String, Int]), v: Vector[String]): (Long, mutable.HashMap[String, Int])  = {
+    (c._1 + 1L, mergeMaps(c._2, v))
+  }
+
+
+  def mergeMaps1(first: mutable.HashMap[String, Int], second: mutable.HashMap[String, Int]) = {
+    second.foreach ( x => {
+      if (first.contains(x._1))
+        first.put(x._1, first(x._1) + x._2)
+      else
+        first.put(x._1, x._2)
+    })
+  }
+
+  def mergeCombiners(c1: (Long,  mutable.HashMap[String, Int]), c2: (Long, mutable.HashMap[String, Int])) = {
+    (c1._1 + c2._1, mergeMaps1(c1._2, c2._2))
+  }
+  val aggregated = data.map ( x => (x._1, x._2)).combineByKey(createCombiner, mergeValue, mergeCombiners)
+
+//  aggregated.foreach (x => println(x._1 + " " + x._2._1 + " " + x._2._2))
+
+//    mergeValue = (c: (Long, DenseVector), v: Vector) => {
+//      BLAS.axpy(1.0, v, c._2)
+//      (c._1 + 1L, c._2)
+//    },
+//    mergeCombiners = (c1: (Long, DenseVector), c2: (Long, DenseVector)) => {
+//      BLAS.axpy(1.0, c2._2, c1._2)
+//      (c1._1 + c2._1, c1._2)
+//    }
+//  ).collect().sortBy(_._1)
+
+//  val finalval = data.filter (x => targetClasses.contains(x._1)).map ( x => (x._1, x._2.split("""[\s\W]""").filter (_.length > 0).map (_.toLowerCase).foldLeft(Map.empty[String, Int]) {
+//    (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
+//  }))
 
   var temp = yStream.flatMap(x => x.split(",")).filter ( x => targetClasses.contains(x)).countByValue()
   val totalCount = temp.map (x => x._2).sum
   val classProbability = temp.map ( x => (x._1, math.log10(x._2.toDouble/totalCount)))
   //endregion
 }
+
 
