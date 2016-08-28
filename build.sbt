468aa48a89d81055ee685b8e8c939634110e6858
musmannisar <usman@uga.edu>
2016-08-28 16:07:37 -0400

    Modified the code, making it much more efficient

--- build.sbt
+++ build.sbt
@@ -1,9 +1,22 @@
-name := "DocClassification"
+lazy val root = (project in file(".")).
+  settings(
+    name := "DocClassification",
+    version := "1.0",
+    scalaVersion := "2.10.5",
+    mainClass in Compile := Some("main")
+  )
 
-version := "1.0"
 
-scalaVersion := "2.11.8"
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.0.0",
+  "org.apache.spark" %% "spark-sql" % "2.0.0",
+  "org.apache.spark" %% "spark-mllib" % "2.0.0"
+)
 
-
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.0"
-libraryDependencies += "org.scala-lang" % "scala-library" % scalaVersion.value
+// META-INF discarding
+mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
+{
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
+}
--- src/main/scala/naiveBayes.scala
+++ src/main/scala/naiveBayes.scala
@@ -1,55 +1,90 @@
-import java.util
+import java.io.PrintWriter
 
-import com.github.fommil.netlib.BLAS
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
-import scala.collection.immutable.HashMap
-import scala.collection.mutable
+import scala.collection.{Map, mutable}
 
 /**
   * Created by UNisar on 8/20/2016.
   */
 class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) extends java.io.Serializable {
-
   /**
     * This method is passed a document as an array of words that runs Naive Bayes algorithm and returns the single target class with the highest score
     *
     * @param words Describes the document as an array of words
     * @return The target class
     */
-  def getScoreForTargetType(words: Array[String]): String =  {
-    val results = targetClasses.map ( target => (target, classProbability(target) + words.map ( word => getProbabilityOfWordInTarget(word, target)).reduceLeft(_+_)))
-    results.maxBy(_._2)._1
+  def getScoreForTargetType(words: Array[String]) =  targetClasses.map ( target => (target, (classProbability.get(target) + words.map ( word => getProbabilityOfWordInTarget(word, target)).reduceLeft(_+_)))).maxBy(_._2)._1
+
+  val tokenizer = """[\s\W]"""      // The tokenizer to split the strings, TODO: improve this
+  val classProbability = new java.util.HashMap[String, Double]    // This structure holds the mapping from CLASS -> P(V)
+
+  /**
+    * Inefficient method get the length of a given class
+ *
+    * @param targetType
+    * @return
+    */
+  def getTotalWords(targetType: String): Double = {
+    var total = 0.0
+    for ( (k,v) <- aggregated(targetType)._2)
+      total += v
+    total
   }
 
-  var finalTrainingResult: scala.collection.Map[String, Map[String, Int]] = null
   /**
     * Primarily, this method fills up the following datastructure: finalTrainingResult
     * This is a map(x1) of map(x2)[String, Int], where x1 is the class, x2 is the word and Int is the count
     */
   def train() {
-//    finalTrainingResult = finalval.collectAsMap()
+    aggregated = corpusIterator.map (x => (x._1, x._2)).combineByKey(createCombiner, mergeValue, mergeCombiners).collectAsMap
+
+    var numberOfDocuments = 0L
+    aggregated.foreach ( numberOfDocuments += _._2._1 )
+
+    // normalizing all the values
+    normalizationValues = targetClasses.map ( x => (x, aggregated(x)._2.maxBy(b => b._2)._2)).toMap
+    totalWordsPerClass = targetClasses.map (x => (x, getTotalWords(x))).toMap
+
+    targetClasses.foreach ( target => classProbability.put(target, (aggregated(target)._1.toDouble/numberOfDocuments)))
   }
 
   /**
     * This method performs the classification for all the documents and then dump the results in an output file
     */
-  def classify(): Unit =
-  {
-//    testData.map ( document => getScoreForTargetType(document.split("""[\s\W]""").filter (_.length>0).map(_.toLowerCase))).saveAsTextFile("Results")
+  def classify(): Unit = {
+    val results = testData.map(document => {
+      getScoreForTargetType(document.split(tokenizer).filter(_.length > 0).filter (c => !stopWordList.contains(c)).map(_.toLowerCase).toSet.intersect(vocabulary).toArray)
+    }).coalesce(1).saveAsTextFile("8 - Path here")
   }
 
+  var normalizationValues: Map[String, Double] = null
+  var totalWordsPerClass: Map[String, Double] = null
   //region Helper Methods
 
   def getCountInTarget (word: String, targetType: String):Double = {
     try {
-      return finalTrainingResult(targetType)(word)
+      return aggregated(targetType)._2(word)
     }
     catch {
       case e:Exception => return 0
     }
   }
+
+  /**
+    * Returns the total occurrences of a given word in the whole corpus
+ *
+    * @param word
+    * @return
+    */
+  def totalCounts(word: String): Double = {
+    var total = 0.0
+    for (t <- targetClasses)
+      total += getCountInTarget(word, t)
+    total
+  }
+
   /**
     * This method calculates the P (w|v) value where w is a word and v is a target value
     *
@@ -57,92 +92,87 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     * @param targetType Target class that we are interested in
     * @return Returns a double value
     */
-  def getProbabilityOfWordInTarget(word: String, targetType: String): Double = {
-    val value = math.log10((getCountInTarget(word, targetType) + 1) / (finalTrainingResult(targetType).size + vocabularySize)) // P(wk|vj)
-    value
-  }
-  //endregion
+  def getProbabilityOfWordInTarget(word: String, targetType: String) = math.log10((getCountInTarget(word, targetType) + 1) / (totalWordsPerClass(targetType) + vocabulary.size))  // P(wk|vj)
+
+  /**
+    * Helper method to determine if the passed string is a number
+ *
+    * @param text
+    * @return
+    */
+  def isAllDigits(text: String) = text forall Character.isDigit
 
   //region DATA reading stuff, pretty straightforward
+  val stopWordList = sc.textFile("StopWordList.txt").collect.toSet
+  var vocabulary = sc.textFile(x).flatMap ( x => x.split(tokenizer)).filter (_.length > 0).map ( _.toLowerCase()).distinct.collect.
+    filter ( !isAllDigits(_)).filter( c => !stopWordList.contains(c)).toSet
+  sc.broadcast(vocabulary)
 
-  val termDocsRdd = sc.textFile(x)
-  //  val vocabulary = termDocsRdd.flatMap(y => y.split(" ")).map(Word(_)).distinct().collect()
-  val vocabularySize = termDocsRdd.flatMap(y => y.split(" ")).distinct().count()
-  val sentinelValue = 1.0/vocabularySize
+  var aggregated: Map[String, (Long, Map[String, Double])] = null
   val xStream = sc.textFile(x)
   val yStream = sc.textFile(y)
   val testStream = sc.textFile(testInput)
-  var testData = for (xs <- testStream) yield xs
-
-  val first = xStream.zipWithIndex().map(x => (x._2, x._1))
-  val second = yStream.zipWithIndex().map ( x => (x._2, x._1))
-  val finalout = first.cogroup(second).map ( x => (x._2._1, x._2._2))
-  val finalStream =  for (x <- finalout)  yield new Tuple2(x._2.head, x._1.head)
-
+  var testData:RDD[String] = for (xs <- testStream) yield xs
+  val documents = xStream.zipWithIndex().map(x => (x._2, x._1))
+  val labels = yStream.zipWithIndex().map (x => (x._2, x._1))
+  val merger = documents.cogroup(labels).map (x => (x._2._1, x._2._2))
+  val mergeIterator = for (x <- merger)  yield new Tuple2(x._2.head, x._1.head)
   val targetClasses = Set("CCAT", "ECAT", "GCAT", "MCAT")
-  val data:RDD[(String, Vector[String])] = finalStream.flatMap ( x =>
+  val corpusIterator:RDD[(String, Vector[String])] = mergeIterator.flatMap (x =>
   {
     val targetTypes =  x._1.split(",").toSet.intersect(targetClasses)
     if (targetTypes.isEmpty)
       None
     else
-      targetTypes.map ( b => (b, x._2.split(" ").toVector))
+      targetTypes.map ( b => (b, x._2.split(tokenizer).filter (_.length > 0).map ( _.toLowerCase).toSet.intersect(vocabulary).toVector))
   })
 
-  def getMap(vals: Vector[String]): mutable.HashMap[String, Int] = {
-    val map = new mutable.HashMap[String, Int]()
-    vals.foreach ( v => map.put(v, 1))
+  /**
+    * Creates a map for the passed strings
+ *
+    * @param vals
+    * @return
+    */
+  def getMap(vals: Vector[String]): mutable.Map[String, Double] = {
+    var map = mutable.Map[String, Double]()
+    for ( i <- vals) map.put(i, 1)
     map
   }
 
-  def mergeMaps(output: mutable.HashMap[String, Int], input: Vector[String]): mutable.HashMap[String, Int] = {
-    input.foreach ( v => output.put(v, output.getOrElse(v, 0) + 1))
-    output
+  def mergeMaps(output: mutable.Map[String, Double], input: Vector[String])= input.foreach ( x => output.put(x, output.getOrElse(x, 0.toDouble) + 1))
+
+  /**
+    * Creates a combiner against a new key for the main combineByKey method
+ *
+    * @param values
+    * @return
+    */
+  def createCombiner(values: Vector[String]): (Long, mutable.Map[String, Double]) = (1, getMap(values))
+
+  /**
+    * This method merges an entry against an existing Combiner
+ *
+    * @param combiner
+    * @param values
+    * @return
+    */
+  def mergeValue(combiner: (Long, mutable.Map[String, Double]), values: Vector[String])= {
+    mergeMaps(combiner._2, values)
+    (combiner._1 + 1, combiner._2)
   }
 
-  def createCombiner(vals: Vector[String]): (Long, mutable.HashMap[String, Int]) = {
-    (1L, getMap(vals))
+  /**
+    * This method merges two existing combiners
+ *
+    * @param firstCombiner
+    * @param secondCombiner
+    * @return
+    */
+  def mergeCombiners(firstCombiner: (Long, mutable.Map[String, Double]), secondCombiner: (Long, mutable.Map[String, Double])) = {
+    mergeMaps(firstCombiner._2, secondCombiner._2)
+    (firstCombiner._1 + secondCombiner._1, firstCombiner._2)
   }
 
-  def mergeValue(c: (Long, mutable.HashMap[String, Int]), v: Vector[String]): (Long, mutable.HashMap[String, Int])  = {
-    (c._1 + 1L, mergeMaps(c._2, v))
-  }
+  def mergeMaps(output: mutable.Map[String, Double], input: mutable.Map[String, Double]) = for ((k,v) <- input) output.put(k, output.getOrElse(k, 0.toDouble) + v)
 
-
-  def mergeMaps1(first: mutable.HashMap[String, Int], second: mutable.HashMap[String, Int]) = {
-    second.foreach ( x => {
-      if (first.contains(x._1))
-        first.put(x._1, first(x._1) + x._2)
-      else
-        first.put(x._1, x._2)
-    })
-  }
-
-  def mergeCombiners(c1: (Long,  mutable.HashMap[String, Int]), c2: (Long, mutable.HashMap[String, Int])) = {
-    (c1._1 + c2._1, mergeMaps1(c1._2, c2._2))
-  }
-  val aggregated = data.map ( x => (x._1, x._2)).combineByKey(createCombiner, mergeValue, mergeCombiners)
-
-//  aggregated.foreach (x => println(x._1 + " " + x._2._1 + " " + x._2._2))
-
-//    mergeValue = (c: (Long, DenseVector), v: Vector) => {
-//      BLAS.axpy(1.0, v, c._2)
-//      (c._1 + 1L, c._2)
-//    },
-//    mergeCombiners = (c1: (Long, DenseVector), c2: (Long, DenseVector)) => {
-//      BLAS.axpy(1.0, c2._2, c1._2)
-//      (c1._1 + c2._1, c1._2)
-//    }
-//  ).collect().sortBy(_._1)
-
-//  val finalval = data.filter (x => targetClasses.contains(x._1)).map ( x => (x._1, x._2.split("""[\s\W]""").filter (_.length > 0).map (_.toLowerCase).foldLeft(Map.empty[String, Int]) {
-//    (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
-//  }))
-
-  var temp = yStream.flatMap(x => x.split(",")).filter ( x => targetClasses.contains(x)).countByValue()
-  val totalCount = temp.map (x => x._2).sum
-  val classProbability = temp.map ( x => (x._1, math.log10(x._2.toDouble/totalCount)))
-  //endregion
 }
-
-
