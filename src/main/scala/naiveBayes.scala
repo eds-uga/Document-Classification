d1eae7ca39bb424bdbabf88c1bf8e44677f04919
joanff <joanff@qq.com>
2016-08-31 18:22:30 -0400

    fixed the train2 classify2 method

--- src/main/scala/naiveBayes.scala
+++ src/main/scala/naiveBayes.scala
@@ -37,13 +37,31 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     */
   def train() {
     aggregated = corpusIterator.map(x => (x._1, x._2)).combineByKey(createCombiner, mergeValue, mergeCombiners).collectAsMap
-    var numberOfDocuments = 0L
     aggregated.foreach(numberOfDocuments += _._2._1)
     // normalizing all the values
     normalizationValues = targetClasses.map(x => (x, aggregated(x)._2.maxBy(b => b._2)._2)).toMap
     totalWordsPerClass = targetClasses.map(x => (x, getTotalWords(x))).toMap
     targetClasses.foreach(target => classProbability.put(target, (aggregated(target)._1.toDouble / numberOfDocuments)))
+  }
+  def train2(): Unit ={
     getTfIdf();
+    corpusIterator2 = corpusIterator.map(T => {
+      (T._1,T._2.map(w => {
+        val specificTFIDF = if(TFIDF.filter(x=>x._1==T._1).filter(x=>x._2==w).isEmpty)  0
+        else TFIDF.filter(x=>x._1==T._1).filter(x=>x._2==w).map(x=>x._3).head
+        specificTFIDF}))})
+    aggregated2 = corpusIterator2.map(x => (x._1, x._2)).combineByKey(createCombiner2, mergeValue2, mergeCombiners2).collectAsMap
+  }
+
+  /**
+    * This method calculates the every term in vocabulary and store in TFIDP which is in form of(TARGET,TERM,TFIDF)
+    */
+  def getTfIdf(): Unit = {
+    TFIDF = for (T <- targetClasses; v <- vocabulary) yield {
+    val temp = aggregated.get(T).filter(x => x._2.keySet.contains(v))
+    val noOfDoc = if (temp == None) 0 else temp.get._1
+    (T, v, (getProbabilityOfWordInTarget(v, T) + math.log10(numberOfDocuments) - math.log10(noOfDoc + 1)))
+    }
   }
 
   /**
@@ -94,28 +112,15 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     */
   def isAllDigits(text: String) = text forall Character.isDigit
 
-  /**
-    * This method calculates the every term in vocabulary and store in TFIDP which is in form of(TARGET,TERM,TFIDF)
-    */
-
-  var TFIDF: Set[(String, String, Double)] =null
-  def getTfIdf(): Unit = {
-    val number0fDocsInLibs = documents.count
-    TFIDF = for (T <- targetClasses; v <- vocabulary) yield (T, v, getProbabilityOfWordInTarget(v, T) + math.log10(number0fDocsInLibs) - math.log10(documents.filter(x => x._2.contains(v)).count() + 1))
-
-  }
 
   // TFIDF clssifier
   def classify2(): Unit = {
-    val results = testData.map(document => {
-      getScoreForTargetType(document.split(tokenizer).filter(_.length > 0).filter(c => !stopWordList.contains(c)).map(_.toLowerCase).toSet.intersect(vocabulary).toArray)
-    }).coalesce(1).saveAsTextFile("/Users/yangfan/Documents/shannondata/result")
-  }
+    val results2 = testData.map(document => {
+      getScoreForTargetType2(document.split(tokenizer).filter(_.length > 0).filter(c => !stopWordList.contains(c)).map(_.toLowerCase))}).coalesce(1).saveAsTextFile("/Users/yangfan/Documents/shannondata/result")
+      }
+
   // get score for a doc in TFIDF way
 
-  /*
-   *   get weighted TfIdf for a specific Word in specific Target
-   */
   def getSinglegetTfIdf(Target1:String,Word1:String):Double =
   {  val SpecificTfIdf = if(TFIDF.filter(x=>x._1==Target1).filter(x=>x._2==Word1).isEmpty)  0
   else TFIDF.filter(x=>x._1==Target1).filter(x=>x._2==Word1).map(x=>x._3).head
@@ -125,23 +130,80 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
   def getScoreForTargetType2(words: Array[String])
   = targetClasses.map(target => (target, (classProbability.get(target) + words.map(word => getSinglegetTfIdfPos(target,word)).reduceLeft(_ + _)))).maxBy(_._2)._1
 
+
   /*
- *   get weighted TfIdf  Posibility for a specific Word in specific Target P(TfIdf/ weighted # of words in Target)
- */
+   *   get weighted TfIdf  Posibility for a specific Word in specific Target P(TfIdf/ weighted # of words in Target)
+   */
   def getSinglegetTfIdfPos(targetType: String,word: String) :Double = (getSinglegetTfIdf(targetType,word)+1) / (getTotalWords2(targetType)) //+ vocabulary.size) // P(wk|vj)
-  // total weights of words in Target Doc in Training
 
   /*  weighted # of words in Target
    *
    */
   def getTotalWords2(targetType: String): Double = {
-    val maptemp:Map[String,Double]= aggregated.get(targetType).get._2
-    val total = {for((k,v)<-maptemp)
-      yield {(maptemp.get(k).get) * getSinglegetTfIdf(targetType,k)}}.foldLeft(0:Double){(m:Double,n:Double) => m+n}
-    total
+    val total = aggregated2.get(targetType).get._2.keySet.foldLeft(0:Double){(m:Double,n:Double) => m+n}
+  total
   }
 
+  /**
+    * Creates a map for the passed TFIDF
+    *
+    * @param vals
+    * @return
+    */
+  def getMap2(vals: Vector[Double]): mutable.Map[Double, Long] = {
+    var map = mutable.Map[Double, Long]()
+    for (i <- vals) {map.put(i,map.getOrElse(i,0:Long)+1:Long)}
+    map
+  }
 
+  /**
+    * Creates a combiner against a new key for the main combineByKey method
+    *
+    * @param values
+    * @return
+    */
+  def createCombiner2(values: Vector[Double]): (Long, mutable.Map[Double, Long]) = (1, getMap2(values))
+  
+  def mergeMaps2(output: mutable.Map[Double, Long], input: Vector[Double]) = input.foreach(x => output.put(x, output.getOrElse(x, 0.toLong) + 1))
+
+  /**
+    * Creates a combiner against a new key for the main combineByKey method
+    *
+    * @param values
+    * @return
+    */
+
+  /**
+    * This method merges an entry against an existing Combiner
+    *
+    * @param combiner
+    * @param values
+    * @return
+    */
+  def mergeValue2(combiner: (Long, mutable.Map[Double, Long]), values: Vector[Double]) = {
+    mergeMaps2(combiner._2, values)
+    (combiner._1 + 1, combiner._2)
+  }
+
+  /**
+    * This method merges two existing combiners
+    *
+    * @param firstCombiner
+    * @param secondCombiner
+    * @return
+    */
+  def mergeCombiners2(firstCombiner: (Long, mutable.Map[Double, Long]), secondCombiner: (Long, mutable.Map[Double, Long])) = {
+    mergeMaps2(firstCombiner._2, secondCombiner._2)
+    (firstCombiner._1 + secondCombiner._1, firstCombiner._2)
+  }
+
+  def mergeMaps2(output: mutable.Map[Double, Long], input: mutable.Map[Double, Long]) = for ((k,v) <- input) output.put(k, output.getOrElse(k, 0.toLong) + v)
+
+
+  var TFIDF: Set[(String, String, Double)] =null
+  var numberOfDocuments = 0L
+  var aggregated2: Map[String, (Long, Map[Double, Long])] = null
+  var corpusIterator2:RDD[(String,Vector[Double])] =null
   //region DATA reading stuff, pretty straightforward
   val tokenizer = """[\s\W]"""  // The tokenizer to split the strings, TODO: improve this
   val classProbability = new java.util.HashMap[String, Double] // This structure holds the mapping from CLASS -> P(V)
@@ -166,6 +228,7 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     else
       targetTypes.map(b => (b, x._2.split(tokenizer).filter(_.length > 0).map(_.toLowerCase).filter ( f => vocabulary.contains(f)).toVector))
   })
+
 
 
   /**
@@ -217,3 +280,8 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
   def mergeMaps(output: mutable.Map[String, Double], input: mutable.Map[String, Double]) = for ((k,v) <- input) output.put(k, output.getOrElse(k, 0.toDouble) + v)
 
 }
+
+
+
+
+
