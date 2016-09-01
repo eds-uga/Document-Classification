c3b3c993d609f5139db53a31649ae1161683ac14
musmannisar <usman@uga.edu>
2016-09-01 00:53:27 -0400

    Implementing TFIDF

--- src/main/scala/naiveBayes.scala
+++ src/main/scala/naiveBayes.scala
@@ -8,7 +8,7 @@ import scala.collection.{Map, mutable}
 /**
   * Created by UNisar on 8/20/2016.
   */
-class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) extends java.io.Serializable {
+class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String, useTFIDF: Boolean) extends java.io.Serializable {
 
   /**
     * This method is passed a document as an array of words that runs Naive Bayes algorithm and returns the single target class with the highest score
@@ -16,7 +16,7 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     * @param words Describes the document as an array of words
     * @return The target class
     */
-  def getScoreForTargetType(words: Array[String]) = targetClasses.map(target => (target, (classProbability.get(target) + words.map(word => getProbabilityOfWordInTarget(word, target)).reduceLeft(_ + _)))).maxBy(_._2)._1
+  def getScoreForTargetType(words: Array[String]) = targetClasses.map(target => (target, (math.log10(classProbability.get(target)) + words.map(word => getProbabilityOfWordInTarget(word, target)).reduceLeft(_ + _)))).maxBy(_._2)._1
 
   /**
     * Inefficient method get the length of a given class
@@ -27,7 +27,7 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
   def getTotalWords(targetType: String): Double = {
     var total = 0.0
     for ((k, v) <- aggregated(targetType)._2)
-      total += v
+      total += v._1
     total
   }
 
@@ -38,28 +38,33 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
   def train() {
     aggregated = corpusIterator.map(x => (x._1, x._2)).combineByKey(createCombiner, mergeValue, mergeCombiners).collectAsMap
 
-    var numberOfDocuments = 0L
     aggregated.foreach(numberOfDocuments += _._2._1)
 
     // normalizing all the values
-    normalizationValues = targetClasses.map(x => (x, aggregated(x)._2.maxBy(b => b._2)._2)).toMap
+    normalizationValues = targetClasses.map(x => (x, aggregated(x)._2.maxBy(b => b._2)._2._1)).toMap
     totalWordsPerClass = targetClasses.map(x => (x, getTotalWords(x))).toMap
 
     targetClasses.foreach(target => classProbability.put(target, (aggregated(target)._1.toDouble / numberOfDocuments)))
   }
 
+  var numberOfDocuments = 0L
+  var wordCounts: Map[String, Int] = null
   /**
     * This method performs the classification for all the documents and then dump the results in an output file
     */
   def classify(): Unit = {
-    val results = testData.map(document => {
-      getScoreForTargetType(document.split(tokenizer).filter(_.length > 0).filter(c => !stopWordList.contains(c)).map(_.toLowerCase).toSet.intersect(vocabulary).toArray)
-    }).coalesce(1).saveAsTextFile("8 - Path here")
+    val results = testData.flatMap(document => {
+      val words = document.split(tokenizer).filter(_.length > 0).filter(c => !stopWordList.contains(c)).map(_.toLowerCase).toSet.intersect(vocabulary).toArray
+      if (!words.isEmpty)
+        Some(getScoreForTargetType(words))
+      else
+        None
+    }).coalesce(1).saveAsTextFile("9 - tfidfcf")
   }
 
   def getCountInTarget(word: String, targetType: String): Double = {
     try {
-      return aggregated(targetType)._2(word)
+      return aggregated(targetType)._2(word)._1
     }
     catch {
       case e: Exception => return 0
@@ -74,8 +79,10 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     */
   def totalCounts(word: String): Double = {
     var total = 0.0
-    for (t <- targetClasses)
-      total += getCountInTarget(word, t)
+    for (t <- targetClasses) {
+      if (aggregated(t)._2.contains(word))
+        total += aggregated(t)._2(word)._2
+    }
     total
   }
 
@@ -86,7 +93,16 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     * @param targetType Target class that we are interested in
     * @return Returns a double value
     */
-  def getProbabilityOfWordInTarget(word: String, targetType: String) = math.log10((getCountInTarget(word, targetType) + 1) / (totalWordsPerClass(targetType) + vocabulary.size)) // P(wk|vj)
+  def getProbabilityOfWordInTarget(word: String, targetType: String): Double = {
+    if (useTFIDF)
+      {
+        val tf = math.log10(getCountInTarget(word, targetType)/totalWordsPerClass(targetType))
+        val idf = math.log10(math.log10(numberOfDocuments/totalCounts(word)))
+        return tf + idf
+      }
+    else
+      return math.log10(((getCountInTarget(word, targetType) + 1)/ (totalWordsPerClass(targetType) + vocabulary.size)))
+  } // P(wk|vj)
 
   /**
     * Helper method to determine if the passed string is a number
@@ -96,32 +112,17 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     */
   def isAllDigits(text: String) = text forall Character.isDigit
 
-  /**
-    * This method calculates the every term in vocabulary and store in TFIDP which is in form of(TARGET,TERM,TFIDF)
-    */
-  //tf-idf
-//  def getTFIDF(): Unit = {
-//    //    val IFij = getProbabilityOfWordInTarget(word,targetType)
-//    val number0fDocsInLibs = xStream.count
-//    //    val numberOfDocsInLibsHasWord = first.filter( x=> x._2.contains(word)).count()
-//    //    val IDFi =math.log10( number0fDocsInLibs) -math.log10 (number0fDocsInLibs +1)
-//    val TFIDF = targetClasses.map(T => vocabulary.map(v => (T, v, getProbabilityOfWordInTarget(v, T) + math.log10(number0fDocsInLibs) - math.log10(xStream.filter(x => x._2.contains(v)).count() + 1))))
-//    //println("DEBUGTFIDF"+TFIDF)
-//
-//  }
-
-
   //region DATA reading stuff, pretty straightforward
   val tokenizer = """[\s\W]"""  // The tokenizer to split the strings, TODO: improve this
   val classProbability = new java.util.HashMap[String, Double] // This structure holds the mapping from CLASS -> P(V)
   var normalizationValues: Map[String, Double] = null
   var totalWordsPerClass: Map[String, Double] = null
-  val stopWordList = sc.textFile("StopWordList.txt").collect.toSet
-  var vocabulary = sc.textFile(x).flatMap(x => x.split(tokenizer)).filter(_.length > 0).map(_.toLowerCase()).distinct.collect.
-    filter(!isAllDigits(_)).filter(c => !stopWordList.contains(c)).toSet
+  val stopWordList = sc.textFile("MergedList.txt").collect.toSet
+  var vocabulary = sc.textFile(x).flatMap(x => x.split(tokenizer)).filter(_.length > 0).map(_.toLowerCase()).distinct.collect
+      .filter(!isAllDigits(_)).filter(c => !stopWordList.contains(c)).toSet
   sc.broadcast(vocabulary)
 
-  var aggregated: Map[String, (Long, Map[String, Double])] = null
+  var aggregated: Map[String, (Long, Map[String, (Double, Double)])] = null
   var testData =  sc.textFile(testInput)
   val documents = sc.textFile(x).zipWithIndex().map(x => (x._2, x._1))
   val labels = sc.textFile(y).zipWithIndex().map(x => (x._2, x._1))
@@ -142,13 +143,21 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     * @param vals
     * @return
     */
-  def getMap(vals: Vector[String]): mutable.Map[String, Double] = {
-    var map = mutable.Map[String, Double]()
-    for (i <- vals) map.put(i, map.getOrElse(i, 0.toDouble) + 1)
+  def getMap(vals: Vector[String]): mutable.Map[String, (Double, Double)] = {
+    var map = mutable.Map[String, (Double, Double)]()
+    for (i <- vals) map.put(i, (map.getOrElse(i, (0.0, 0.0))._1 + 1, 1))
     map
   }
 
-  def mergeMaps(output: mutable.Map[String, Double], input: Vector[String]) = input.foreach(x => output.put(x, output.getOrElse(x, 0.toDouble) + 1))
+  def mergeMaps(output: mutable.Map[String, (Double, Double)], input: Vector[String]) = {
+    input.foreach(x => {
+      output.put(x, (output.getOrElse(x, (0.0, 0.0))._1 + 1, output.getOrElse(x, (0.0, 0.0))._2))
+    })
+
+    input.distinct.foreach(x => {
+      output.put(x, (output.getOrElse(x, (0.0, 0.0))._1, output.getOrElse(x, (0.0, 0.0))._2 + 1))
+    })
+  }
 
   /**
     * Creates a combiner against a new key for the main combineByKey method
@@ -156,7 +165,7 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     * @param values
     * @return
     */
-  def createCombiner(values: Vector[String]): (Long, mutable.Map[String, Double]) = (1, getMap(values))
+  def createCombiner(values: Vector[String]): (Long, mutable.Map[String, (Double, Double)]) = (1, getMap(values))
 
   /**
     * This method merges an entry against an existing Combiner
@@ -165,7 +174,7 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     * @param values
     * @return
     */
-  def mergeValue(combiner: (Long, mutable.Map[String, Double]), values: Vector[String]) = {
+  def mergeValue(combiner: (Long, mutable.Map[String, (Double, Double)]), values: Vector[String]) = {
     mergeMaps(combiner._2, values)
     (combiner._1 + 1, combiner._2)
   }
@@ -177,11 +186,13 @@ class naiveBayes (sc: SparkContext, x: String, y:String, testInput: String) exte
     * @param secondCombiner
     * @return
     */
-  def mergeCombiners(firstCombiner: (Long, mutable.Map[String, Double]), secondCombiner: (Long, mutable.Map[String, Double])) = {
+  def mergeCombiners(firstCombiner: (Long, mutable.Map[String, (Double, Double)]), secondCombiner: (Long, mutable.Map[String, (Double, Double)])) = {
     mergeMaps(firstCombiner._2, secondCombiner._2)
     (firstCombiner._1 + secondCombiner._1, firstCombiner._2)
   }
 
-  def mergeMaps(output: mutable.Map[String, Double], input: mutable.Map[String, Double]) = for ((k,v) <- input) output.put(k, output.getOrElse(k, 0.toDouble) + v)
+  def mergeMaps(output: mutable.Map[String, (Double, Double)], input: mutable.Map[String, (Double, Double)]) = {
+    for ((k,v) <- input) output.put(k, (output.getOrElse(k, (0.0, 0.0))._1 + v._1, output.getOrElse(k, (0.0, 0.0))._2 + v._2))
+  }
 
 }
